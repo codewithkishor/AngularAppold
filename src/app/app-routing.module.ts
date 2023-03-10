e3566ee502f3d4a82d1cba8e2a97a00e79eaca23
codewithkishor <bkishor306@gmail.com>
2023-03-10 14:39:34 +0530

    Assignment
    All changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: '', redirectTo:'home', pathMatch:'full'}, // http:localhost:4200 
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent }, // http://localhost:4200/aboutus
+  { path: 'contactus', component: ContactusComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
+  
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,16 @@ import { CustomeComponent } from './custome/custome.component';
 import { TemplateformComponent } from './templateform/templateform.component';
 import { UserTableComponent } from './user-table/user-table.component';
 import { PipeexampleComponent } from './pipeexample/pipeexample.component';
-import { CustompipePipe } from './custompipe.pipe';
+// import { CustompipePipe } from './custompipe.pipe';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgClass, NgComponentOutlet, NgForOf, NgIf, NgPluralCase, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
 
 
 @NgModule({
@@ -21,16 +28,28 @@ import { Comp2Component } from './comp2/comp2.component';
     TemplateformComponent,
     UserTableComponent,
     PipeexampleComponent,
-    CustompipePipe,
     Comp1Component,
-    Comp2Component
+    Comp2Component,
+    AboutusComponent,
+    ContactusComponent,
+    HomeComponent,
+    UserdetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgClass,
+
   
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+ 
+  
+
+}
